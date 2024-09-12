f7fe0c0bf5d4964d9ea2a7cb3384273397e1f1b2
MinaYasser100 <minayasser43@gmail.com>
2024-09-12 16:25:46 +0300

    add some constant

---
+++ lib/core/constant/color/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromARGB(255, 68, 177, 255);
+  static const Color secondColor = Color.fromARGB(255, 142, 210, 249);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/features/login/presentation/views/login_view.dart';
+import 'package:chat_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,15 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: const Locale('ar'),
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
     );
   }
 }
