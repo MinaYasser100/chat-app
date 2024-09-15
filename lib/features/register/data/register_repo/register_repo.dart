8e8e7e92cb0777db4ea9d52f5c92b78eb84cf9f7
MinaYasser100 <minayasser43@gmail.com>
2024-09-15 23:24:00 +0300

    do the method for process register

--- lib/features/register/data/register_repo/register_repo.dart
+++ lib/features/register/data/register_repo/register_repo.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
 
+import 'package:chat_app/core/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 abstract class RegisterRepo {
   Future<Reference> uploadImageOnFirebase(File image);
+  Future<void> putUserInformationInFirebase({
+    required UserModel userModel,
+  });
+  Future<UserCredential> registerUser(
+      {required String email, required String password});
 }
--- lib/features/register/data/register_repo/register_repo_implement.dart
+++ lib/features/register/data/register_repo/register_repo_implement.dart
@@ -1,10 +1,15 @@
 import 'dart:io';
 
+import 'package:chat_app/core/constant/title/titles.dart';
+import 'package:chat_app/core/model/user_model.dart';
 import 'package:chat_app/features/register/data/register_repo/register_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RegisterRepoImplement implements RegisterRepo {
   final FirebaseStorage _storage = FirebaseStorage.instance;
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
   @override
   Future<Reference> uploadImageOnFirebase(File image) async {
     Reference storageReference = _storage
@@ -17,5 +22,22 @@ class RegisterRepoImplement implements RegisterRepo {
       print('Image uploaded successfully!');
     });
     return storageReference;
+  }
+
+  @override
+  Future<void> putUserInformationInFirebase(
+      {required UserModel userModel}) async {
+    await _firestore
+        .collection(Titles.userCollection)
+        .doc()
+        .set(userModel.toMap());
+  }
+
+  @override
+  Future<UserCredential> registerUser(
+      {required String email, required String password}) async {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return userCredential;
   }
 }
