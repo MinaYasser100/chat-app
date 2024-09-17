3b49677dda746090340214882e92ddac0186ec88
MinaYasser100 <minayasser43@gmail.com>
2024-09-17 16:40:48 +0300

    do home app bar

---
+++ lib/features/home/presentation/views/func/custom_home_app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:chat_app/core/constant/color/app_colors.dart';
+import 'package:chat_app/core/constant/styles/styles.dart';
+import 'package:chat_app/core/helper/hive/hive_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+AppBar customHomeAppBar() {
+  return AppBar(
+    backgroundColor: AppColors.primaryColor,
+    foregroundColor: Colors.white,
+    leadingWidth: 70,
+    leading: Padding(
+      padding: const EdgeInsets.only(right: 10, bottom: 5),
+      child: CircleAvatar(
+        backgroundImage: NetworkImage(HiveHelper.getUserData().image),
+      ),
+    ),
+    title: FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Text(
+        _getFirstTwoWords(HiveHelper.getUserData().name),
+        style: Styles.textStyle24White.copyWith(fontWeight: FontWeight.w700),
+      ),
+    ),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(FontAwesomeIcons.gear),
+      ),
+    ],
+  );
+}
+
+String _getFirstTwoWords(String fullName) {
+  List<String> words = fullName.split(' ');
+  if (words.length > 1) {
+    // Join the first two words
+    return '${words[0]} ${words[1]} ${words[2]}';
+  } else {
+    // If the name has less than two words, return the full name
+    return fullName;
+  }
+}
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,13 +1,16 @@
 import 'package:chat_app/features/home/presentation/views/widgets/home_body_view.dart';
 import 'package:flutter/material.dart';
 
+import 'func/custom_home_app_bar.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeBodyView(),
+    return Scaffold(
+      appBar: customHomeAppBar(),
+      body: const HomeBodyView(),
     );
   }
 }
