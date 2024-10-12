083a7100f3defe7a3fb952fd138ff88f3ba71541
MinaYasser100 <minayasser43@gmail.com>
2024-10-12 17:14:03 +0300

    handeling the show messages

--- lib/features/home/presentation/views/widgets/home_body_view.dart
+++ lib/features/home/presentation/views/widgets/home_body_view.dart
@@ -17,42 +17,63 @@ class HomeBodyView extends StatefulWidget {
   State<HomeBodyView> createState() => _HomeBodyViewState();
 }
 
-class _HomeBodyViewState extends State<HomeBodyView> {
+class _HomeBodyViewState extends State<HomeBodyView>
+    with WidgetsBindingObserver {
   TextEditingController messageController = TextEditingController();
   final UserModel userModel = HiveHelper.getUserData();
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    super.didChangeMetrics();
+    final bottomInset = View.of(context).viewInsets.bottom;
+    if (bottomInset > 0) {
+      _scrollToBottom();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const SizedBox(
-          height: 10,
-        ),
-        BlocBuilder<MessagesCubit, MessagesState>(
-          builder: (context, state) {
-            if (state is MessagesLoaded) {
-              return Expanded(
-                child: ListView.builder(
+        const SizedBox(height: 10),
+        Expanded(
+          child: BlocBuilder<MessagesCubit, MessagesState>(
+            builder: (context, state) {
+              if (state is MessagesLoaded) {
+                return ListView.builder(
+                  controller: _scrollController,
                   itemCount: state.messages.length,
                   itemBuilder: (context, index) {
                     final message = state.messages[index];
                     return message.sender.userId == userModel.userId
-                        ? SenderMessageItem(
-                            messageModel: message,
-                          )
-                        : ReceiverMessageItem(
-                            messageModel: message,
-                          );
+                        ? SenderMessageItem(messageModel: message)
+                        : ReceiverMessageItem(messageModel: message);
                   },
-                ),
-              );
-            } else if (state is MessagesError) {
-              return Center(child: Text('Error: ${state.error}'));
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
+                );
+              } else if (state is MessagesError) {
+                return Center(child: Text('Error: ${state.error}'));
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
         Container(
           clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -72,15 +93,30 @@ class _HomeBodyViewState extends State<HomeBodyView> {
               controller: messageController,
             ),
             suffixOnTap: () {
-              context.read<MessagesCubit>().sendMessage(
-                    content: messageController.text,
-                    sender: userModel,
-                  );
-              messageController.clear();
+              if (messageController.text.isNotEmpty) {
+                context.read<MessagesCubit>().sendMessage(
+                      content: messageController.text,
+                      sender: userModel,
+                    );
+                messageController.clear();
+                _scrollToBottom();
+              }
             },
           ),
         ),
       ],
     );
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent + 100,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      }
+    });
   }
 }
